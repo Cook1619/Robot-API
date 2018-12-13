67adc85605cb72a67558d23520e2e54b92036285
Matt <cook6348@gmail.com>
2018-12-13 08:25:31 -0600

    Updates

---
+++ precache-manifest.9254a216816370634ca66ffd68303ac7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robot-API/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "cdfdfd28a2b5c13121be",
+    "url": "/Robot-API/static/js/runtime~main.cdfdfd28.js"
+  },
+  {
+    "revision": "73a090f3fd507be3c108",
+    "url": "/Robot-API/static/js/main.73a090f3.chunk.js"
+  },
+  {
+    "revision": "46e4e14d005c615e1b53",
+    "url": "/Robot-API/static/js/1.46e4e14d.chunk.js"
+  },
+  {
+    "revision": "73a090f3fd507be3c108",
+    "url": "/Robot-API/static/css/main.5f658fb5.chunk.css"
+  },
+  {
+    "revision": "46e4e14d005c615e1b53",
+    "url": "/Robot-API/static/css/1.6d672b99.chunk.css"
+  },
+  {
+    "revision": "b5b7ad9ef96f904ca3e54712195b8915",
+    "url": "/Robot-API/index.html"
+  }
+];
